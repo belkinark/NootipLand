38a8e911d04297573c6afba8f2ff61c4c8c0e983
belkinark <67878832+belkinark@users.noreply.github.com>
2022-04-28 14:20:51 +0500

    Add files via upload

--- wtf.js
+++ wtf.js
@@ -11,3 +11,12 @@ let boton = document.querySelector(".boton")
 boton.onclick = () => {
 	window.open('https://www.donationalerts.com/r/nootipland', 'Сайт оплаты', 'width=600,height=900, top='+((screen.height-900)/2)+',left='+((screen.width-600)/2));
 };
+
+let buy = document.querySelector("div[adapt]")
+document.addEventListener("DOMContentLoaded", function(event){
+	var widthWind = document.querySelector('body').offsetWidth;
+	console.log(widthWind)
+	if (widthWind <= 995){
+		buy.classList.remove("col")
+	}
+});
